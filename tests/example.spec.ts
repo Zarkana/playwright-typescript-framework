c52ac07b06412d45e95a711bc02e61d2b5478ed6
Joseph Campbell <jsc940@gmail.com>
2025-02-02 08:10:04 -0700

    ab testing and add remove elements tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,23 +1,19 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('can add element', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/The Internet/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+// test('get started link', async ({ page }) => {
+//   await page.goto('https://the-internet.herokuapp.com/');
 
-  await page.getByRole('link', { name: 'Get started' })
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+//   await page.getByRole('link', { name: 'Get started' })
+//   // Click the get started link.
+//   await page.getByRole('link', { name: 'Get started' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-
-test('search hello google', async ({ page}) => {
-  
-})
+//   // Expects page to have a heading with the name of Installation.
+//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+// });
--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,6 +1,36 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  
+test('Delete button appears', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/add_remove_elements/');
+  await expect(page.getByRole('button', { name: 'Delete' })).not.toBeVisible();
+  await page.getByRole('button', { name: 'Add Element' }).click();
+  await expect(page.getByRole('button', { name: 'Delete' })).toBeVisible();
+});
 
+test('Delete button disappears', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/add_remove_elements/');
+  await page.getByRole('button', { name: 'Add Element' }).click();
+  await expect(page.getByRole('button', { name: 'Delete' })).toBeVisible();
+  await page.getByRole('button', { name: 'Delete' }).click();
+  await expect(page.getByRole('button', { name: 'Delete' })).not.toBeVisible();
+});
+
+test('Multiple delete button appear', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/add_remove_elements/');
+  await expect(page.getByRole('button', { name: 'Delete' })).not.toBeVisible();
+  await page.getByRole('button', { name: 'Add Element' }).click();
+  await page.getByRole('button', { name: 'Add Element' }).click();
+  await expect(page.getByRole('button', { name: 'Delete' })).toHaveCount(2);
+});
+
+test('Multiple delete button can be removed', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/add_remove_elements/');
+  await expect(page.getByRole('button', { name: 'Delete' })).not.toBeVisible();
+  await page.getByRole('button', { name: 'Add Element' }).click();
+  await page.getByRole('button', { name: 'Add Element' }).click();
+  await expect(page.getByRole('button', { name: 'Delete' })).toHaveCount(2);
+  await page.getByRole('button', { name: 'Delete' }).nth(1).click();
+  await expect(page.getByRole('button', { name: 'Delete' })).toHaveCount(1);
+  await page.getByRole('button', { name: 'Delete' }).nth(0).click();
+  await expect(page.getByRole('button', { name: 'Delete' })).not.toBeVisible();
 });
