046a0432e2466cd687f847006787ee8d7641b23b
Joseph Campbell <jsc940@gmail.com>
2025-02-02 19:46:52 -0700

    fix challenging dom test

--- tests/challenging-dom.spec.ts
+++ tests/challenging-dom.spec.ts
@@ -6,7 +6,7 @@ test('challenging dom produces three buttons of type foo, bar, baz, or qux', asy
   const barCount = await page.getByRole('link', { name: 'bar', exact: false }).count();
   const bazCount = await page.getByRole('link', { name: 'baz', exact: false }).count();
   const quxCount = await page.getByRole('link', { name: 'qux', exact: false }).count();
-  const total = fooCount + barCount + bazCount;
+  const total = fooCount + barCount + bazCount + quxCount;
   expect(fooCount).toBeLessThan(4);
   expect(barCount).toBeLessThan(4);
   expect(bazCount).toBeLessThan(4);
